901775f9041686781a2772cc30bad0ba86d33ca2
Randy W. Sims <simsrw73@gmail.com>
2023-01-07 14:15:20 -0500

    Initial commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+require("user.options")
+require("user.plugins")
+require("user.keymaps")
+require("user.autocmds")
+
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
